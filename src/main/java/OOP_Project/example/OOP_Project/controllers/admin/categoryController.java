cf8b3c51b9467c910a6f6391adcc243b936172f2
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-10-14 17:23:05 +0700

    thêm upload file cho category

--- src/main/java/OOP_Project/example/OOP_Project/controllers/admin/categoryController.java
+++ src/main/java/OOP_Project/example/OOP_Project/controllers/admin/categoryController.java
@@ -1,7 +1,11 @@
 package OOP_Project.example.OOP_Project.controllers.admin;
 
 import OOP_Project.example.OOP_Project.models.categoryModel;
+import OOP_Project.example.OOP_Project.services.StorageService;
 import OOP_Project.example.OOP_Project.services.categoryService;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +15,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/admin")
@@ -18,6 +24,9 @@ public class categoryController {
 
     @Autowired
     private categoryService cateService;
+
+    @Autowired
+    private StorageService storageService;
 
     @GetMapping("/category")
     public String index(Model model) {
@@ -38,7 +47,12 @@ public class categoryController {
     }
 
     @PostMapping("/category/add")
-    public String addPost(@ModelAttribute("category") categoryModel category) {
+    public String addPost(@ModelAttribute("category") categoryModel category, @RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            this.storageService.store(file);
+            System.out.println(file);
+            category.setImage(file.getOriginalFilename());
+        }
         if (this.cateService.create(category)) {
             return "redirect:/admin/category";
         } else {
@@ -57,7 +71,25 @@ public class categoryController {
     }
 
     @PostMapping("/category/edit/{id}")
-    public String editPost(@ModelAttribute("category") categoryModel category, @PathVariable("id") Integer id) {
+    public String editPost(@ModelAttribute("category") categoryModel category, @PathVariable("id") Integer id, @RequestParam("file") MultipartFile file) {
+        categoryModel cateExist = this.cateService.findById(id);
+        if (!file.isEmpty()) {
+            // Xóa ảnh cũ nếu có
+            String oldImagePath = cateExist.getImage();
+            if (oldImagePath != null && !oldImagePath.isEmpty()) {
+                Path oldImage = storageService.load(oldImagePath);
+                try {
+                    Files.deleteIfExists(oldImage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            // Lưu ảnh mới
+            storageService.store(file);
+            category.setImage(file.getOriginalFilename());
+        } else {
+            category.setImage(cateExist.getImage());
+        }
         if (this.cateService.update(category)) {
             return "redirect:/admin/category";
         } else {
