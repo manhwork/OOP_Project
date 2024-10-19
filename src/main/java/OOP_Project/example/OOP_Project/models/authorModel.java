93b30ccc73621508209286d6d3692dca8d79bd51
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-10-19 23:27:30 +0700

    update author

--- src/main/java/OOP_Project/example/OOP_Project/models/authorModel.java
+++ src/main/java/OOP_Project/example/OOP_Project/models/authorModel.java
@@ -1,24 +1,22 @@
 package OOP_Project.example.OOP_Project.models;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.Date;
 import java.util.Random;
 
 @Entity
-@Table(name = "authur")
+@Table(name = "author")
 
 public class authorModel {
 
     @Id
     @Column(name = "id")
 //    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id= new Random().nextInt(Integer.MAX_VALUE);;
+    private Integer id = new Random().nextInt(Integer.MAX_VALUE);
+    ;
 
     @Column(name = "name")
     private String name;
@@ -34,6 +32,16 @@ public class authorModel {
     private Date creatAt = new Date();
     @Column(name = "updateAt")
     private Date updateAt = new Date();
+    @Column(name = "image")
+    private String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 
     public authorModel() {
         // TODO Auto-generated constructor stub
@@ -115,6 +123,3 @@ public class authorModel {
     }
 
 }
-
-
-
--- src/main/java/OOP_Project/example/OOP_Project/repository/authorRepository.java
+++ src/main/java/OOP_Project/example/OOP_Project/repository/authorRepository.java
@@ -1,7 +1,6 @@
 package OOP_Project.example.OOP_Project.repository;
 
 import OOP_Project.example.OOP_Project.models.authorModel;
-import OOP_Project.example.OOP_Project.models.bookModel;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,5 +8,5 @@ import org.springframework.data.jpa.repository.Query;
 public interface authorRepository extends JpaRepository<authorModel, Integer> {
 
     @Query("SELECT b FROM authorModel b WHERE b.is_exist = true")
-    List<bookModel> find();
+    List<authorModel> find();
 }
