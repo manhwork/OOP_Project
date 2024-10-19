e5dedbd406e1dbe2ec5cc39a60c35f07a33af6d8
Chuongg <dochuong2811@gmail.com>
2024-10-20 00:13:17 +0700

    thêm readmore blog và shoppingCart

--- src/main/java/OOP_Project/example/OOP_Project/controllers/client/BlogCtrl.java
+++ src/main/java/OOP_Project/example/OOP_Project/controllers/client/BlogCtrl.java
@@ -13,9 +13,22 @@ public class BlogCtrl {
     @GetMapping("/blog")
     public String blog(ModelMap model) {
         model.addAttribute("pageTitle","Blog");
-        return "client/blog.html";
+        return "client/blog/blog.html";
     }
      
+    @GetMapping("/blog/Blog1")
+    public String getBlog1(){
+        return "client/blog/Blog1";
+    }
     
+    @GetMapping("/blog/Blog2")
+    public String getBlog2(){
+        return "client/blog/Blog2";
+    }
+    
+    @GetMapping("/blog/Blog3")
+    public String getBlog3(){
+        return "client/blog/Blog3";
+    }
     
 }
