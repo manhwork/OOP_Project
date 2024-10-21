94877caa0b512a53deb835dfb48a922ed6ad2bde
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-10-21 16:40:25 +0700

    show books in client

--- src/main/java/OOP_Project/example/OOP_Project/controllers/client/BooksCtrl.java
+++ src/main/java/OOP_Project/example/OOP_Project/controllers/client/BooksCtrl.java
@@ -1,6 +1,9 @@
-
 package OOP_Project.example.OOP_Project.controllers.client;
 
+import OOP_Project.example.OOP_Project.models.bookModel;
+import OOP_Project.example.OOP_Project.services.bookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class BooksCtrl {
 
+    @Autowired
+    private bookService bookService;
+
     @GetMapping("/books")
     public String book(ModelMap model) {
-        model.addAttribute("pageTitle","Books");
+        List<bookModel> records = this.bookService.getItem();
+        model.addAttribute("records", records);
+        model.addAttribute("pageTitle", "Books");
         return "client/books.html";
     }
-     
-    
-    
+
 }
