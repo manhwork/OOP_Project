b59be03ffa20a188ba397543b35368480c9fd9f9
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-10-23 09:09:59 +0700

    cart

--- src/main/java/OOP_Project/example/OOP_Project/controllers/client/ShoppingCartCtrl.java
+++ src/main/java/OOP_Project/example/OOP_Project/controllers/client/ShoppingCartCtrl.java
@@ -1,21 +1,14 @@
-
 package OOP_Project.example.OOP_Project.controllers.client;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class ShoppingCartCtrl {
 
     @GetMapping("/ShoppingCart")
     public String getShoppingCart() {
-        
         return "client/ShoppingCart.html";
     }
-     
-    
-    
+
 }
